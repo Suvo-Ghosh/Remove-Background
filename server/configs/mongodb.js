598680802cfd4a75ff46055cdbd2c24dd5927f27
Suvo Ghosh <suvosh663@gmail.com>
2025-01-21 15:08:34 +0530

    .

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () => {
         console.log("Dtabase Connected")
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}remove`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/remove`)
 }
 
 export default connectDB
